93f832a58497b5786f7f580a3459299c8b7d147a
Phil Underwood <beardydoc@gmail.com>
2023-05-29 23:58:15 +0100

    Make calibration more user friendly, but still work to do

--- firmware/calibrate.py
+++ firmware/calibrate.py
@@ -1,18 +1,25 @@
 import mag_cal
 from async_button import Button
-
+try:
+    import numpy as np
+except ImportError:
+    from ulab import numpy as np
 import display
 import hardware
 import config
-
+import json
 
 async def calibrate(devices: hardware.Hardware, cfg: config.Config, disp: display.Display):
+    devices.laser_enable(True)
     disp.show_info("Take a series of\r\nreadings, press\r\nA for a leg,\r\nB to finish")
     await devices.button_a.wait(Button.SINGLE)
+    await devices.laser.set_laser(True)
     mags = []
     gravs = []
     count = 0
     while True:
+        disp.show_info(f"Legs: {count}\r\nIdeally at least 24\r\nWith two groups\r\nin same " +
+                       f"direction")
         button, click = await devices.both_buttons.wait(a=Button.SINGLE, b=Button.SINGLE)
         if button == "a":
             mags.append(devices.magnetometer.magnetic)
@@ -20,10 +27,14 @@ async def calibrate(devices: hardware.Hardware, cfg: config.Config, disp: displa
         elif button == "b":
             break
         count += 1
-        disp.show_info(f"Legs: {count}\r\nIdeally at least 24\r\nWith two groups\r\nin same " +
-                       f"direction")
     cal = mag_cal.Calibration(mag_axes=cfg.mag_axes, grav_axes=cfg.grav_axes)
-    accuracy = cal.calibrate(mags, gravs)
+    try:
+        #save data if we can
+        with open("calibration_data.json","w") as f:
+            json.dump({"mag": mags, "grav": gravs}, f)
+    except OSError:
+        pass
+    accuracy = cal.calibrate(np.array(mags), np.array(gravs),mag_cal.Calibration.AXIS_CORRECTION)
     if accuracy < 0.25:
         quality = "excellent"
     elif accuracy < 0.5:
@@ -32,7 +43,7 @@ async def calibrate(devices: hardware.Hardware, cfg: config.Config, disp: displa
         quality = "acceptable"
     else:
         quality = "poor"
-    report = f"Accuracy is {accuracy:.3f}°\r\nThis is {quality}\r\nPress A to Save\nB to Discard"
+    report = f"Accuracy is {accuracy:.3f}°\r\nThis is {quality}\r\nPress A to Save\r\nB to Discard"
     disp.show_info(report)
     button, click = await devices.both_buttons.wait(a=Button.SINGLE, b=Button.SINGLE)
     if button == "a":
