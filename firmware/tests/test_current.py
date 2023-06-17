a18e078ee9409c45834a06d4954e6d72730d4423
Phil Underwood <beardydoc@gmail.com>
2023-06-17 23:24:59 +0100

    Limited code to test battery current consumption

---
+++ firmware/tests/test_current.py
@@ -0,0 +1,25 @@
+import alarm
+import pins
+import digitalio
+import _bleio
+import board
+import time
+import hardware
+import asyncio
+
+async def runner():
+    with hardware.Hardware() as dev:
+        await asyncio.sleep(0.3)
+
+LIGHT_SLEEP_TIMEOUT = 60
+_bleio.adapter.stop_advertising()
+time.sleep(0.1)
+asyncio.run(runner())
+pin_alarm = alarm.pin.PinAlarm(pins.BUTTON_A, value=False, pull=True)
+periph_alarm = alarm.pin.PinAlarm(pins.PERIPH_EN, value=True, pull=True)
+usb_alarm = alarm.pin.PinAlarm(board.CHARGE_STATUS, value=False, pull=False)
+#wakeup = alarm.light_sleep_until_alarms(pin_alarm, usb_alarm)
+#print(wakeup.pin)
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + LIGHT_SLEEP_TIMEOUT)
+print("sleeping")
+wakeup = alarm.light_sleep_until_alarms(time_alarm, pin_alarm, usb_alarm)
