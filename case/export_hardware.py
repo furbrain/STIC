547d73569d8e3053f2b725008cf0a4cc25352ab5
Phil Underwood <beardydoc@gmail.com>
2024-07-24 15:38:27 +0100

    Fix bug introduced in last commit

--- case/export_hardware.py
+++ case/export_hardware.py
@@ -9,10 +9,10 @@ doc = App.getDocument("bigboy")
 time.sleep(3)
 stl_parts = ("shim", "cap", "cap with vanes", "mount", "shell", "bezel")
 os.makedirs("build/abs/",exist_ok=True)
-#for part_name in stl_parts:
-#    print(f"Exporting {part_name}") 
-#    part = doc.findObjects(Label=f"^{part_name}$")[0]
-#    Mesh.export([part],f"build/abs/{part_name}.stl")
+for part_name in stl_parts:
+    print(f"Exporting {part_name}") 
+    part = doc.findObjects(Label=f"^{part_name}$")[0]
+    Mesh.export([part],f"build/abs/{part_name}.stl")
     
 drawings = doc.findObjects("TechDraw::DrawPage")
 drawing_stls = []
